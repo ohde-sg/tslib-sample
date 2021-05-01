0dd0182cca28d649430283a05a07cad441415c3e
yoshiyuki ohde <y_ohde@odproductions.jp>
2021-05-02 01:46:54 +0900

    tmp

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
+#!/usr/bin/env node
+
 import {add} from './add'
 
 let arr = process.argv.slice(2).map(x => parseInt(x))
 let sum = 0
 for(const x of arr){
-  sum += x
+  sum = add(sum,x)
 }
 console.log(sum)
-console.log("wow")
